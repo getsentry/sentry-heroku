89d808a0006eb228289cc949f214132c5d22743c
David Cramer <dcramer@gmail.com>
2015-09-17 15:43:42 -0700

    Record owner

--- src/sentry_heroku/plugin.py
+++ src/sentry_heroku/plugin.py
@@ -12,9 +12,19 @@ from sentry.plugins import ReleaseHook, ReleaseTrackingPlugin
 
 class HerokuReleaseHook(ReleaseHook):
     def handle(self, request):
+        email = request.POST['user']
+        try:
+            user = User.objects.get(
+                email__iexact=email,
+                sentry_orgmember_set__organization__project=self.project,
+            )
+        except User.MultipleObjectsReturned:
+            user = None
+
         self.finish_release(
             version=request.POST['head_long'],
             url=request.POST['url'],
+            owner=user,
         )
 
 
